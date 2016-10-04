ee99a1d1e717ddf9a3c4c44082fc33321b813d29
MossFrog <kaanozcelik@sabanciuniv.edu>
2016-10-04 14:02:18 +0200

    Updated the main file to render the head section of the snake, structural changes.

--- Main.cpp
+++ Main.cpp
@@ -3,15 +3,46 @@
 
 using namespace std;
 
+//-- Struct for storing sections of the Snake (Blocks) --//
+struct snakeSection
+{
+	sf::Vector2f position;
+	sf::RectangleShape render;
+	sf::RectangleShape innerRender;
+};
+
+void updateRender(snakeSection & actualSection);
+
 int main()
 {
 	//-- Set the resolution to 16:9 preferred resolution for landscape mobile phones --//
 	//-- Disable keyrepeat events to make the game tap oriented --//
 	//-- Limit the framerate to prevent screen tearing (although this does cause dirty pixels) --//
-	sf::RenderWindow window(sf::VideoMode(720, 450), "Snake - SFML", sf::Style::Close);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Snake - SFML", sf::Style::Close);
 	window.setFramerateLimit(120);
-	window.setKeyRepeatEnabled(true);
+	window.setKeyRepeatEnabled(false);
 
+
+	//-- Definitions Section --//
+	string mainDirection = "East";
+
+	//-- rectangleShapes used as a default renders for each snakeSection --//
+	sf::RectangleShape defaultRect;
+	defaultRect.setSize(sf::Vector2f(20, 20));
+	defaultRect.setOrigin(10, 10);
+
+	sf::RectangleShape defaultInnerRect;
+	defaultInnerRect.setSize(sf::Vector2f(18, 18));
+	defaultInnerRect.setOrigin(9, 9);
+	defaultInnerRect.setFillColor(sf::Color::Black);
+
+	//-- Definition of the snake "head" --//
+	snakeSection snakeHead;
+	snakeHead.position = sf::Vector2f(100, 100);
+	snakeHead.render = defaultRect;
+	snakeHead.innerRender = defaultInnerRect;
+
+	//------------------------//
 
 	while (window.isOpen())
 	{
@@ -21,7 +52,6 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 
-			//-- If the specified key is pressed push the player upwards --//
 			if (event.type == sf::Event::KeyPressed)
 			{
 
@@ -52,15 +82,23 @@ int main()
 		sf::Vector2i mousePos = sf::Mouse::getPosition(window);
 
 
+		//-- Update all the rendered elements --//
+		updateRender(snakeHead);
+
 		window.clear(sf::Color::Black);
 
 
-		//window.draw(playerShape);
+		window.draw(snakeHead.render);
+		window.draw(snakeHead.innerRender);
 
 		window.display();
 	}
 
-
-
 	return 0;
+}
+
+void updateRender(snakeSection & actualSection)
+{
+	actualSection.render.setPosition(actualSection.position);
+	actualSection.innerRender.setPosition(actualSection.position);
 }
